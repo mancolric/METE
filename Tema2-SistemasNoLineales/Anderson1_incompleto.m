1c84e2fccbc2a29425ad67d19293fd82519b0481
mancolric <mancolric@gmail.com>
2026-03-05 11:53:47 +0100

    update

--- Tema2-SistemasNoLineales/Anderson1_incompleto.m
+++ Tema2-SistemasNoLineales/Anderson1_incompleto.m
@@ -4,62 +4,62 @@ function [x_k, nIters, flag] = Anderson1(gfun, x0, Tol, MaxIter, M)
 
     %Comprobamos que x0 es vector columna:
     if size(x0,2)>1
-        error('????')
+        error(XX)
     end
     %Aseguramos que M<=N
-    N           = ???;
-    M           = min(??, ??); 
+    N           = XX;
+    M           = min(XX, XX); 
     
     %Primera iteraci'on:
-    nIters      = ??;
-    x_k         = ??;                   %soluci'on
-    g_k         = ??;            %residuo precondicionado    
-    p_k         = ??;                 %paso
-    Xm          = ??;           %matrices con info 'ultimos pasos
-    Gm          = ??;
+    nIters      = XX;
+    x_k         = XX;                   %soluci'on
+    g_k         = XX;            %residuo precondicionado    
+    p_k         = XX;                 %paso
+    Xm          = XX;           %matrices con info 'ultimos pasos
+    Gm          = XX;
     while true
 
         %Salimos del bucle:
-        if norm(p_k)<=???
-            flag    = ???;
+        if norm(p_k)<=XX
+            flag    = XX;
             break
-        elseif nIters==???
-            flag   = ???;
+        elseif nIters==XX
+            flag   = XX;
             break
         end
 
         %Actualizamos x_nm1 y g_nm1:
-        x_km1       = ???;
-        g_km1       = ???;
+        x_km1       = XX;
+        g_km1       = XX;
         
         %Actualizamos x_n y g_n:
-        nIters      = ???
-        x_k         = ???
-        g_k         = ???
+        nIters      = XX
+        x_k         = XX
+        g_k         = XX
         
         %Actualizamos matrices X y G:
-        Deltag_norm = norm(???) + 1e-12;
-        Xm(:,2:M)   = ???;
-        Xm(:,1)     = ???/Deltag_norm;
-        Gm(:,2:M)   = ???
-        Gm(:,1)     = ???/Deltag_norm;
+        Deltag_norm = norm(XX) + 1e-12;
+        Xm(:,2:M)   = XX;
+        Xm(:,1)     = XX/Deltag_norm;
+        Gm(:,2:M)   = XX
+        Gm(:,1)     = XX/Deltag_norm;
         
         %Factorizaci'on en valores singulares de G:
-        [U,S,V]                     = svd(???,0);   %S=Sigma
+        [U,S,V]                     = svd(XX,0);   %S=Sigma
         
         %Pseudoinversa de S:
-        S_diag                      = diag(???);
-        S_psinv_diag                = 1.0./???;
-        S_psinv_diag(S_diag<???)    = ???;
-        S_psinv                     = diag(???);
+        S_diag                      = diag(XX);
+        S_psinv_diag                = 1.0./XX;
+        S_psinv_diag(S_diag<XX)     = XX;
+        S_psinv                     = diag(XX);
         
         %Calculamos gamma = V Sigma^+ U^T g^n:
-        gamma                       = ???;
+        gamma                       = XX;
         
         %#Pr'oximo paso. Aplicamos f'ormula multisecante:
         %   pn  = - H g^n = - [I + (X-G) (G^T G)^{-1} G^T] g^n 
         %       = -g^n - X*gamma + G*gamma,
-        p_k                         = ???;
+        p_k                         = XX;
         
     end
     
--- Tema2-SistemasNoLineales/Polin_Anderson_incompleto.m
+++ Tema2-SistemasNoLineales/Polin_Anderson_incompleto.m
@@ -1,45 +1,45 @@
 function Polin_Anderson(N) 
 
     %Aproximaci'on inicial (OJO! Definir como vector COLUMNA):
-    x0                  = ones(??,??);
+    x0                  = ones(XX,XX);
     
     %Ponemos el cronómetro en marcha:
     t1                  = tic();
     
     %Calculamos y factorizamos el Jacobiano en x0:
-    [~,J0]              = Polin_residuo(??, ??);
-    [??]                = lu(J0);
+    [~,J0]              = Polin_residuo(XX, XX);
+    [XX]                = lu(J0);
     
     %Definimos función residuo precondicionado:
     function g=gfun(x)
-        [f,~]           = ???
-        g               = ???
+        [f,~]           = XX(XX)
+        g               = XX
     end
 
     %Reolvemos con Anderson:
-    [x, nIters, flag]   = Anderson1(???, ???, ???, ???, ???);
+    [x, nIters, flag]   = Anderson1(XX, XX, XX, XX, XX);
     if flag<0
-        warning('???')
+        warning('XX')
     end
     
     %Paramos cron'metro:
-    t2                  = toc(??);
+    t2                  = toc(XX);
         
     %Soluci'on exacta (como vector columna) y error en norma infinita:
-    x_ex                = ??;
-    err                 = norm(??,Inf);
+    x_ex                = XX;
+    err                 = norm(XX,Inf);
     
     %Gr'aficas:
     figure()
-    plot(??, '+-b')
+    plot(XX, '+-b')
     hold on
-    plot(??, 'r')
+    plot(XX, 'r')
     
     %Info por pantalla:
-    display(?)
-    display(?)
-    display(?)
-    display(?)
+    display(XX)
+    display(XX)
+    display(XX)
+    display(XX)
 
 end
 
