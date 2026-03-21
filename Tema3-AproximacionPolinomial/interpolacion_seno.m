4aa9efe544212276b07d2e00545a9c3dbe60ece0
mancolric <mancolric@gmail.com>
2026-03-21 15:37:54 +0100

    tema 3 - clase 20/3

---
+++ Tema3-AproximacionPolinomial/interpolacion_seno.m
@@ -0,0 +1,30 @@
+function interpolacion_seno(N)
+
+    %Datos:
+    fun     = @(x) sin(1.0*x);
+    a       = pi/2;
+    b       = pi;
+    
+    %Nodos de interpolaci'on:
+    x_int   = linspace(a,b,N+1).';
+    
+    %Calculamos coeficientes:
+    cv      = PolyVander1(x_int, a, b, N)\fun(x_int);
+    
+    %Nodos de evaluaci'on. Valor de la funci'on y del polinomio de
+    %interpolaci'on en los mismos:
+    x_eval  = linspace(a, b, 100).';    
+    f_eval  = fun(x_eval);
+    p_eval  = PolyVander1(x_eval, a, b, N)*cv;
+    
+    %Representamos la soluci'on:
+    figure()
+    plot(x_eval, f_eval, 'xb')
+    hold on
+    plot(x_eval, p_eval, 'r')
+    plot(x_int, fun(x_int), 'ok')
+    
+    %Error m'aximo:
+    disp(norm(f_eval-p_eval,Inf))
+    
+end
